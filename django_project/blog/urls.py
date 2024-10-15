4258f844be2f00f52caeda1187a8584edf595e93
Jae hyeon <wjs2063@naver.com>
2024-10-15 23:13:43 +0900

    feat : add blog css & views.py & html

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from django.contrib import admin
+from . import views
 
+app_name = 'blog'
 urlpatterns = [
-    path('blog', include('blog.urls')),
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
 ]
 
 
